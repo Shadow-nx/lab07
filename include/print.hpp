1b0f8a19c274afd0c9ec3dd4cdbca5a0916ffc38
shadow <shadow-nx@mail.ru>
2018-05-28 18:01:44 +0300

    added documentation

--- include/print.hpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <fstream>
-#include <iostream>
-
-void print(const std::string& text, std::ostream& out = std::cout);
-//function print 
-void print(const std::string& text, std::ofstream& out);
-//overload function print
